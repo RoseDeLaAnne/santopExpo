826ac6b8d52c619abc65382bc9c6f25375b30807
MWinterE <mwexcellency@outlook.com>
2023-05-20 21:36:46 +0300

    1.2.0

---
+++ assets/js/modal.js
@@ -0,0 +1,9 @@
+document.getElementById("openModalBtn").addEventListener("click", function () {
+    document.getElementById("myModal").style.display = "block";
+});
+
+document
+    .getElementsByClassName("close")[0]
+    .addEventListener("click", function () {
+        document.getElementById("myModal").style.display = "none";
+    });
